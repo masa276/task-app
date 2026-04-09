3cc68259442393f116c64b7623b4f3e5689b9769
masa276 <vividlancer@gmail.com>
2026-04-09 17:15:41 +0900

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,8 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}  # SQLite用
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
