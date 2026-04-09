6031b14d66ad93bb489ee7c498ca06318db674f2
masa276 <vividlancer@gmail.com>
2026-04-09 12:48:31 +0900

    Update task.ts

--- frontend/src/api/task.ts
+++ frontend/src/api/task.ts
@@ -1,1 +1,12 @@
+// src/api/task.ts
+import { client } from "./client";
 
+export const getTasks = async () => {
+  const res = await client.get("/tasks");
+  return res.data;
+};
+
+export const createTask = async (data: { title: string }) => {
+  const res = await client.post("/tasks", data);
+  return res.data;
+};
