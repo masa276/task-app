eef144581f60ba5b547edf2d7e18e93546618470
masa276 <vividlancer@gmail.com>
2026-04-09 12:48:54 +0900

    Update TaskList.tsx

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,1 +1,18 @@
+import { useEffect, useState } from "react";
+import { getTasks } from "../api/task";
 
+export const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasks().then(setTasks);
+  }, []);
+
+  return (
+    <ul>
+      {tasks.map((task: any) => (
+        <li key={task.id}>{task.title}</li>
+      ))}
+    </ul>
+  );
+};
