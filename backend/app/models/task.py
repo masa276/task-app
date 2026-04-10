dc5d2ca3e251911142c3fb6ab32c286e4aac4c1d
masa276 <vividlancer@gmail.com>
2026-04-10 10:32:49 +0900

    Update task.py

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -1,18 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
 from app.db.base import Base
-from app.models.user import User
 
 class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    title = Column(String)
     description = Column(String, nullable=True)
-    completed = Column(Boolean, default=False)
 
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
 
-    # Userとのリレーション
-    owner = relationship("User", back_populates="tasks")
+    # 👇 ここがセット
+    user = relationship("User", back_populates="tasks")
